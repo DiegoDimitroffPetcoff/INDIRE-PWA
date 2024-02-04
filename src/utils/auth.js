d6f96b72530293f9bd4a9ecde32dfbf684feb2c6
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2024-02-04 09:54:38 +0000

    DeletePDFMakers not ussefull anymore

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,17 +1,13 @@
 import { Providers, ProviderState } from "@microsoft/mgt-element";
 export const Auth = async () => {
   if (Providers.globalProvider.state === ProviderState.SignedIn) {
-    console.log(Providers.globalProvider.state);
-    console.log("Providers.globalProvider.state");
-    console.log("ProviderState.SignedIn");
-    console.log(ProviderState.SignedIn);
     try {
       const accessToken = await Providers.globalProvider.getAccessToken({});
       return accessToken;
     } catch (error) {
       return "Error trying to get token";
     }
-  }else{
-    return "User no Logged"
+  } else {
+    return "User no Logged";
   }
 };
