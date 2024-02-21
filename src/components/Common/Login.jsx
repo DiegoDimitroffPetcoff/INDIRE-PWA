6f31f3b26b7d2b9873eedb6a081ead0482049451
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2024-02-21 02:22:03 +0000

    Page 404 css

--- src/components/Common/Login.jsx
+++ src/components/Common/Login.jsx
@@ -11,6 +11,8 @@ export const Log = () => {
         flexWrap: "wrap",
         height: "100vh",
         flexDirection: "column",
+        boxShadow: "rgba(0, 0, 0, 0.5) -1px -1px 20px 0px inset"
+        
       }}
     >
       <Logo />
--- src/components/Common/NotFound.jsx
+++ src/components/Common/NotFound.jsx
@@ -1,3 +1,32 @@
+import { TbError404 } from "react-icons/tb";
+import { Logo } from "./Logo";
+
 export const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignContent: "center",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        style={{
+          boxShadow: "rgba(0, 0, 0, 0.5) -1px -1px 20px 0px inset",
+          borderRadius: "10px",
+          width: "50%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+        <Logo />
+        Not Found
+        <TbError404 />
+      </div>
+    </div>
+  );
 };
